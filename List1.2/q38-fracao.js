a0a73c8f5075399ec4ae5666a8b5263eacce2823
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-03-05 15:54:35 -0300

    frist commit

--- List1.2/q38-fracao.js
+++ List1.2/q38-fracao.js
@@ -1,2 +1,18 @@
 //Leia 2 (duas) frações (numerador e denominador), calcule e escreva a soma destas frações, escrevendo o resultado em forma de fração.
 import {question} from 'readline-sync'
+
+function main () {
+    //Entrada
+    const num1 = Number(question('Numerador1: '))
+    const den1 = Number(question('Denominador1: '))
+    const num2 = Number(question('Numerador2: '))
+    const den2 = Number(question('Denominador2: '))
+
+    //Processamento
+    const denominador = den1 * den2
+    const numerador = ((denominador / den1)*num1) + ((denominador / den2)*num2)
+    
+    //Saída
+    console.log('Fração:', numerador+'/'+denominador)
+}
+main()
