585a825c7bd1e5f6d418a0f8784b9f7d5114ee89
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-03-06 15:42:26 -0300

    frist commit

--- List1.2/q42-pontos.js
+++ List1.2/q42-pontos.js
@@ -3,15 +3,19 @@ ponto2 (x2,y2), escreva a distância entre eles, conforme fórmula abaixo. -VER 
 import {question} from 'readline-sync'
 
 function main () { 
+    //Entrada
     const x1 = Number(question('Valor para x1: '))
     const y1 = Number(question('Valor para y1: '))
-    const x2 = Number(question('Valor para y1: '))
-    const y2 = Number(question('Valor para y1: '))
+    const x2 = Number(question('Valor para x2: '))
+    const y2 = Number(question('Valor para y2: '))
 
-    const distancia_x = (x2 - x1)**2
-    const distancia_y = (y2 - y1)**2
+    //Processamento
+    const x = (x2 - x1)**2
+    const y = (y2 - y1)**2
+    const distancia = Math.sqrt(x + y)
 
-    //const raiz_quadrada = 
+    //Saída
+    console.log(`A distância é de ${distancia.toFixed(2)}`)
 
 }
 main()
