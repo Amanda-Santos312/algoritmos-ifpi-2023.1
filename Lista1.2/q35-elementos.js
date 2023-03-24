f055c836e254b2af2c8b46ebf917cec04534cbbb
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-03-24 10:03:19 -0300

    questões de 24 a 46

--- Lista1.2/q35-elementos.js
+++ Lista1.2/q35-elementos.js
@@ -3,19 +3,21 @@ import {question} from 'readline-sync'
 
 function main () {
     //Entrada
-    const numero = Number(question('Digite um número de quatro dígitos: ')) //1234
+    const numero = Number(question('Numero de quatro dígitos: ')) //1234
 
     //Processamento
-    const uni_milhar = Math.floor(numero / 1000) //1
-    const resto = numero % 1000 //234
-
-    const centena = Math.floor(numero/ 100) //2
-    const dezena = Math.floor(resto / 10)//3
-    const unidade = resto % 10 //4
+    const uni_milhar = Math.floor(numero / 1000) //1.234 -> 1
+    const centena = Math.floor(numero / 100) % 10  //2
+    const dezena = Math.floor(numero/ 10) % 10//3
+    const unidade = numero % 10//4
 
     const soma = uni_milhar + centena + dezena + unidade
 
     //Saída
-    console.log(`A soma dos número é ${soma}`)
+    console.log(`${uni_milhar}`)
+    console.log(`${centena}`)
+    console.log(`${dezena}`)
+    console.log(`${unidade}`)
+    console.log(`\nA soma dos número é ${soma}`)
 }
 main()
