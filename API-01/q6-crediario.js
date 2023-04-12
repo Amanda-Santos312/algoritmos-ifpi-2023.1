032fa5e81c001841e84104b3e9f3d803bdc8a633
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-04-12 09:55:27 -0300

    atividades

---
+++ API-01/q6-crediario.js
@@ -0,0 +1,10 @@
+import {question} from 'readline-sync'
+
+function main() {
+    const valor_iphone = Number(question('Valor do iphone: '))
+    const tipo_pagamento = question('Forma de pagamento: Pix / Debito / ou Entrada+Credito?: ')
+    
+
+}
+
+main()
