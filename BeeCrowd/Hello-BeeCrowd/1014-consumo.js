0b4a2479143c643c3d52acb522f7bd74a0c855d3
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-05-06 19:58:56 -0300

    questões do Beecrowd

--- BeeCrowd/Hello-BeeCrowd/1014-consumo.js
+++ BeeCrowd/Hello-BeeCrowd/1014-consumo.js
@@ -1,8 +1,17 @@
-//
+//Calcule o consumo médio de acordo com a distancia total e o combsutivel gasto.
 import {question} from 'readline-sync'
 
 function main() {
+    const distancia_total = Number(question('\nDistancia total percorrida: '))
+    const combustivel_gasto = Number(question('Total de combustivel gasto: '))
 
+    const consumo_medio = calcula_consumo_medio(distancia_total, combustivel_gasto)
+
+    console.log(`\nConsumo Médio: ${consumo_medio.toFixed(3)} km/l`)
+}
+
+function calcula_consumo_medio(distancia_total, combustivel_gasto) {
+    return distancia_total / combustivel_gasto
 }
 
 main()
