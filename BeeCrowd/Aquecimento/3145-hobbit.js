faa87650a7a89232a73d16d5cd9de3f3e731628d
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-05-19 15:10:45 -0300

    add

---
+++ BeeCrowd/Aquecimento/3145-hobbit.js
@@ -0,0 +1,13 @@
+
+import {obter_texto, mostrar_texto} from '../../API-SC03/input-utils.js'
+
+function main() {
+    const [a, d] = obter_texto().split(' ').map(Number)
+
+    const qtd_pessoas = a + 2
+
+    const dias = d / qtd_pessoas
+
+    mostrar_texto(`${dias.toFixed(2)}`)
+}
+main()
