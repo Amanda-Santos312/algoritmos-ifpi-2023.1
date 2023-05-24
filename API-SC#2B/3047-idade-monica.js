a50fe4c955c592a162dcc2ed57ddfc4e6b485e35
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-05-23 21:41:21 -0300

    questões API 2B

--- API-SC#2B/3047-idade-monica.js
+++ API-SC#2B/3047-idade-monica.js
@@ -1,7 +1,35 @@
-import {} from ''
+import {obter_numero_positivo, mostrar_texto} from '../modulo5-Strings/input-output.js'
 
 function main() {
+    const idade_monica = obter_numero_positivo('Idade de monica: ')
+    const idade_filho1 = obter_numero_positivo('Idade do filho1: ')
+    const idade_filho2 = obter_numero_positivo('Idade do filho2: ')
 
+    const soma_idades = idade_filho1 + idade_filho2
+
+    const idade_filho3 = calcula_idade_do_filho3(idade_monica, soma_idades)
+
+    const mais_velho = idade_filho_mais_velho(idade_filho1, idade_filho2, idade_filho3)
+
+    mostrar_texto(mais_velho)
+}
+
+function calcula_idade_do_filho3(idade_monica, soma_idades) {
+    const x = idade_monica - soma_idades
+    return x
+}
+
+
+function idade_filho_mais_velho(idade_f1, idade_f2, idade_f3) {
+    if (idade_f1 > idade_f2 && idade_f1 > idade_f3) {
+        return idade_f1
+
+    } else if (idade_f2 > idade_f1 && idade_f2 > idade_f3) {
+        return idade_f2
+
+    } else {
+        return idade_f3
+    }
 }
 
 main()
