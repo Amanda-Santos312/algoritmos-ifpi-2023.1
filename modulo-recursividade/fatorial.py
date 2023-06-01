4d807ac672a41a0c5631e1e2926313c12ade6727
Amanda Santos <catce.2022111tads0053@aluno.ifpi.edu.br>
2023-05-31 23:54:55 -0300

    with python

--- modulo-recursividade/fatorial.py
+++ modulo-recursividade/fatorial.py
@@ -1,13 +1,18 @@
-
+# -*- coding: utf-8 -*-
 def main():
     valor = int(input('N: '))
-    
+
     resultado = fatorial(valor)
-    
-    print(f'{valor} fatorial eh igual a {resultado}')
-    
-    
+
+    print('\nFatorial:')
+    print(resultado)
+
+
 def fatorial(numero):
     if (numero == 0):
         return 1
-    return numero * fatorial(numero  - 1)
+
+    return numero * fatorial(numero - 1)
+
+
+main()
